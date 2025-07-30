08e0e72a961373f075eb9efb5bddaf4498062665
Al Bastien <ajbastien@users.noreply.github.com>
2025-07-29 21:15:11 -0400

    renamed elements for shunt

--- renogybt/ShuntClient.py
+++ renogybt/ShuntClient.py
@@ -90,13 +90,13 @@ class ShuntClient(ShuntBaseClient):
     def parse_shunt_info(self, bs):
         data = {}
         #temp_unit = self.config['data']['temperature_unit']
-        data['charge_battery_percent'] = bytes_to_int(bs, 34, 2, scale = 0.1) # 0xA6 (#1)
-        data['charge_battery_voltage'] = bytes_to_int(bs, 25, 3, scale = 0.001) # 0xA6 (#1)
+        data['main_battery_percent'] = bytes_to_int(bs, 34, 2, scale = 0.1) # 0xA6 (#1)
+        data['main_battery_voltage'] = bytes_to_int(bs, 25, 3, scale = 0.001) # 0xA6 (#1)
         data['starter_battery_voltage'] = bytes_to_int(bs, 30, 2, scale = 0.001) # 0xA6 (#2)
-        data['discharge_amps'] = bytes_to_int(bs, 21, 3, scale = 0.001, signed=True) # 0xA4 (#1)
-        data['discharge_watts'] = round((data['charge_battery_voltage'] * data['discharge_amps']), 2)
-        data['temperature_sensor_1'] = 0.00 if bytes_to_int(bs, 67, 1) == 0 else bytes_to_int(bs, 66, 3, scale = 0.001) # 0xAD (#3)
-        data['temperature_sensor_2'] = 0.00 if bytes_to_int(bs, 71, 1) == 0 else bytes_to_int(bs, 70, 3, scale = 0.001) # 0xAD (#4)
+        data['charge_amps'] = bytes_to_int(bs, 21, 3, scale = 0.001, signed=True) # 0xA4 (#1)
+        data['charge_watts'] = round((data['charge_battery_voltage'] * data['discharge_amps']), 2)
+        data['temperature_1'] = 0.00 if bytes_to_int(bs, 67, 1) == 0 else bytes_to_int(bs, 66, 3, scale = 0.001) # 0xAD (#3)
+        data['temperature_2'] = 0.00 if bytes_to_int(bs, 71, 1) == 0 else bytes_to_int(bs, 70, 3, scale = 0.001) # 0xAD (#4)
         # unknown values:
         # - time_remaining
         # - discharge_duration
